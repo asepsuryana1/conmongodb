351b032b23cba56c7004e6c6e52657b0d341a9f0
asepsuryana1 <asepsuryana1@gmail.com>
2020-12-02 11:24:24 +0700

    connect to database success https://www.npmjs.com/package/mongodb

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'myproject';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+  console.log(client);
+ 
+  client.close();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
